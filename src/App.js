3a964e4855342018e81853a8a3632e1332e29429
Mattegbg <90768274+Mattegbg@users.noreply.github.com>
2022-03-30 19:49:40 +0200

    Pages, routes, css etc

--- src/App.js
+++ src/App.js
@@ -16,18 +16,17 @@ function App() {
   return (
 
     //Nedan skapar vi routes -> path -> ID för vår data.js
-    <div className="App">
-
-
+    //Dark i containern kommer från CSS
+    <div className="container dark"> 
+      <div className='app'>
       <Header />
-
       <Routes>
 
         <Route element={<NotesPage />} exact path="/" />
         <Route element={<NotePage />} exact path="/note/:id" />
 
       </Routes>
-
+      </div>
     </div>
 
   );
--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import notes from '../assets/data';
-import ListItem  from '../components/ListItem';
+import ListItem from '../components/ListItem';
 
 const NotesPage = () => {
     return (
-        <div>
-            {notes.map((note, index) =>( //importerar data (array) från -> assets -> data.js
-                <ListItem key={index} note={note}/> //här skickar vi in ett objekt i ListItem komponenten
-            ))}
+        // under där vi skriver "&#9782;" gör att vi får en symbol som liknar en lista eller liknande brevid notes. Bra trick!
+        // Rad 12 "notes-count" gör att vi ser hur många notes vi har på sidan. 
+        <div className='notes'>
 
-            
+            <div className='notes-header'>
+                <h2 className='notes-title'> &#9782;Notes </h2>
+                <p classname='notes-count'>{notes.length}</p>
+            </div>
+
+            <div className='notes-list'>
+
+                {notes.map((note, index) => ( //importerar data (array) från -> assets -> data.js
+                    <ListItem key={index} note={note} /> //här skickar vi in ett objekt i ListItem komponenten
+                ))}
+
+            </div>
+
+
+
+
         </div>
     )
 }
